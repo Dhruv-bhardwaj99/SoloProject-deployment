7c2a04e864fd5a6dc0292ec0941268f4977cb5d7
Dhruv Bhardwaj <96115027+Dhruv-bhardwaj99@users.noreply.github.com>
2022-07-03 16:17:13 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const connect = require("./configs/db");
 require("dotenv").config();
 const cors = require("cors");
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 5000;
+const timeout = require("connect-timeout");
 
 //controller
 const productControllers = require("./controllers/product.controller");
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(timeout('5000ms'))
 app.use("/products", productControllers);
 
 
